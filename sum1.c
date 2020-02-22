ec7ee91e1de12bfe7e66838f922fe9e89cdd49f6
Deepesh <dlad@nseit.com>
2020-02-22 16:19:22 +0530

    second commit

--- sum1.c
+++ sum1.c
@@ -2,10 +2,12 @@
 
 int main()
 {
-	int num1, num2, sum;
+	int num1, num2, sum, sub;
 	printf("Enter the numbers: ");
 	scanf("%d %d", &num1, &num2);
 	sum=num1+num2;
+	sub=num1-num2;
 	printf("The sum of %d and %d is %d", num1, num2, sum);
+	printf("The subtraction of % d and %d is %d", num1, num2, sub);
 	return 0;
 }
